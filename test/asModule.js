34e0e7952b0fdfaa456e788ba7cf4b2b73d4f648
mluemkemann <mluemkemann@arigo.de>
2019-05-13 15:13:12 +0200

    final for now

--- test/asModule.js
+++ test/asModule.js
@@ -2,10 +2,15 @@ const should = require('chai').should();
 const fs = require('fs-extra');
 const { replaceScript } = require('../index.js');
 
+const outDir = 'test/out';
+
 describe('as module', () => {
+	before(() => {
+		fs.mkdirp(outDir);
+	});
 	it('should work on html-import', async () => {
 		const input = 'test/in/web-component.html';
-		const output = 'test/out/web-component.html';
+		const output = outDir + '/web-component.html';
 		const templ = 'test/templ/web-component.html';
 		const script = 'test/in/script.js';
 
@@ -17,7 +22,7 @@ describe('as module', () => {
 	});
 	it('should work on normal html-file', async () => {
 		const input = 'test/in/page.html';
-		const output = 'test/out/page.html';
+		const output = outDir + '/page.html';
 		const templ = 'test/templ/page.html';
 		const script = 'test/in/script.js';
 
