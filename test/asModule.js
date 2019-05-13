0fbb90b0b3c8f7fc8096adf40f9c0c265921acd6
mluemkemann <mluemkemann@arigo.de>
2019-05-13 15:10:30 +0200

    test

--- test/asModule.js
+++ test/asModule.js
@@ -1,12 +1,30 @@
 const should = require('chai').should();
+const fs = require('fs-extra');
 const { replaceScript } = require('../index.js');
-
-const input = 'test/in/web-component.html';
-const output = 'test/out/web-component.html';
-const script = 'test/in/script.js';
 
 describe('as module', () => {
 	it('should work on html-import', async () => {
+		const input = 'test/in/web-component.html';
+		const output = 'test/out/web-component.html';
+		const templ = 'test/templ/web-component.html';
+		const script = 'test/in/script.js';
+
 		await replaceScript({input, output, script});
+
+		const outputContent = await fs.readFile(output, 'utf-8');
+		const templContent = await fs.readFile(templ, 'utf-8');
+		outputContent.should.equal(templContent);
+	});
+	it('should work on normal html-file', async () => {
+		const input = 'test/in/page.html';
+		const output = 'test/out/page.html';
+		const templ = 'test/templ/page.html';
+		const script = 'test/in/script.js';
+
+		await replaceScript({input, output, script});
+
+		const outputContent = await fs.readFile(output, 'utf-8');
+		const templContent = await fs.readFile(templ, 'utf-8');
+		outputContent.should.equal(templContent);
 	});
 });
